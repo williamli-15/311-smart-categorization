165de750515781df181d6a4c46ac167e3984b73e
williamli-15 <li812080076@gmail.com>
2024-07-17 23:17:48 -0400

    removed github page as homepage

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.use(cors());
 
--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { PropTypes } from 'prop-types';
 import React, { Component } from 'react';
-import { Link, BrowserRouter as Router } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import AlertDismissable from './components/AlertDismissable';
 import Routes from './Routes';
@@ -30,41 +30,39 @@ class App extends Component {
 
   render() {
     return (
-      <Router basename="/311-smart-categorization">
-        <div className="App">
-          <Container>
-            <Navbar collapseOnSelect className="app-nav-bar hidden" variant="dark" expand="lg">
-              <Navbar.Brand as={Link} to="/">GSOC Prototype 0703</Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="">
-                  <Link className="nav-link" to="/">Classify</Link>
-                  <Link className="nav-link" to="/about">About</Link>
-                  <Link className="nav-link" to="/frame1">(WIP) Frame 1</Link>
-                  <Link className="nav-link" to="/frame2">Frame 2</Link>
-                  <Link className="nav-link" to="/frame3">Frame 3</Link>
-                  <Link className="nav-link" to="/frame4">Frame 4</Link>
-                  <Link className="nav-link" to="/frame5">Frame 5</Link>
-                </Nav>
-              </Navbar.Collapse>
-            </Navbar>
-            { this.props.updateAvailable && this.state.showUpdateAlert &&
-              <div style={{paddingTop: '10px'}}>
-                <AlertDismissable
-                  title=""
-                  variant="info"
-                  message={this.state.reloadMsg}
-                  show={this.props.updateAvailable && this.state.showUpdateAlert}
-                  onClose={this.dismissUpdateAlert} />
-              </div>
-            }
-          </Container>
-          <Container>
-            <Routes />
-          </Container>
-          <PWAInstallComponent></PWAInstallComponent>
-        </div>
-      </Router>
+      <div className="App">
+        <Container>
+          <Navbar collapseOnSelect className="app-nav-bar hidden" variant="dark" expand="lg">
+            <Navbar.Brand as={Link} to="/">GSOC Prototype 0703</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="">
+                <Link className="nav-link" to="/">Classify</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/frame1">(WIP) Frame 1</Link>
+                <Link className="nav-link" to="/frame2">Frame 2</Link>
+                <Link className="nav-link" to="/frame3">Frame 3</Link>
+                <Link className="nav-link" to="/frame4">Frame 4</Link>
+                <Link className="nav-link" to="/frame5">Frame 5</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          { this.props.updateAvailable && this.state.showUpdateAlert &&
+            <div style={{paddingTop: '10px'}}>
+              <AlertDismissable
+                title=""
+                variant="info"
+                message={this.state.reloadMsg}
+                show={this.props.updateAvailable && this.state.showUpdateAlert}
+                onClose={this.dismissUpdateAlert} />
+            </div>
+          }
+        </Container>
+        <Container>
+          <Routes />
+        </Container>
+        <PWAInstallComponent></PWAInstallComponent>
+      </div>
     );
   }
 }
@@ -73,4 +71,4 @@ App.propTypes = {
   updateAvailable: PropTypes.bool.isRequired,
 };
 
-export default App;
+export default withRouter(App);
